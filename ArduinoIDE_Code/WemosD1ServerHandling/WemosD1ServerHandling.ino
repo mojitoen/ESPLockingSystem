5850d59af0cd98e5b769de4637fe09f821404ddb
Marcus Manuel Feliciano <marcus@feliciano.eu>
2023-11-13 04:48:25 +0100

    0.96 inch OLED and security features
    + Added support for the SSD1306 OLED in Wemos D1 code
    + Reply to GET-requests now returns 401, as no users are supposed to GET the chip. Could probably remove GET-handling altogether, but it could be useful for any troubleshooting
    + OLED now displays IP-address information directly on the screen.
    + POST response now has different "modes" one for a correct answer, and one for wrong answer. Ideally the application should would never be in a situation where the wrong code is sent, so this is to stop just about any-nancy from bruteforcing the locks open with spammable requests. Will have to test this at a later date
    + Added L2CAddress-finder from Rui Santos to the project. Useful tool to establish if any changes have to be made to Addressing, and could be incorporated into the server eventually

--- ArduinoIDE_Code/WemosD1ServerHandling/WemosD1ServerHandling.ino
+++ ArduinoIDE_Code/WemosD1ServerHandling/WemosD1ServerHandling.ino
@@ -2,9 +2,22 @@
 #include <ESP8266WebServer.h>
 #include "secret.h"
 
-//Placeholder Wifi information. Change before you upload to hardware!!
+//Display Imports
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+//Define Display values
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+//Construct Adafruit SSD1306
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+//Wifi information. bases itself off of secret.h
 const char* ssid = SECRET_SSID;
 const char* password = SECRET_WPA;
+const char* unlock_key = SECRET_KEY;
 
 //Init server
 ESP8266WebServer server(80);
@@ -14,15 +27,45 @@ ESP8266WebServer server(80);
 //handleRoot happens whenever the server gets a GET-Request
 void handleRoot() {
   server.sendHeader("Access-Control-Allow-Origin", "*");
-  server.send(200, "text/plain", "Hello World");
+  server.send(401, "text/plain", "This isn't how you're supposed to use this device!");
   blink();
+  display.setCursor(0, 10);
+  display.clearDisplay();
+  display.println("GET-Received");
+  display.display();
 }
+
+//function which sole purpose is to display information
+void displayStatus() {
+    //On Connect: 
+    display.clearDisplay();
+    display.println("Connected to Wifi: ");
+    display.println(WiFi.localIP());
+    display.display();
+    Serial.println("Connected to WiFi. IP-address: ");
+    Serial.println(WiFi.localIP()); 
+    }
+    
 //HandlePost happens whenever the servers gets a POST-request
 void handlePost() {
   server.sendHeader("Access-Control-Allow-Origin", "*");
-  String message = "Hello World (POST)";
-  server.send(200, "text/plain", message);
-  blink();
+  display.setCursor(0, 10);
+  display.clearDisplay();
+  String postData = server.arg("plain");
+  if(postData.indexOf(unlock_key) != -1) {
+    server.send(200, "text/plain", "correct code");
+    display.println("Correct code!");
+    display.display();
+    delay(10000);
+    displayStatus();
+  }
+  else {
+    server.send(401, "text/plain", "NOT AUTHORIZED");
+    display.println("POST-Received but not authenticated");
+    display.display();
+    delay(10000);
+    displayStatus();
+  }
 }
 
 //Function for blinking BUILT-IN LED
@@ -36,22 +79,52 @@ void blink() {
 void setup() {
   Serial.begin(19200);
   pinMode(BUILTIN_LED, OUTPUT);
-  
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println("Error allocating memory ");
+    }
+  delay(2000);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+
 
 
   // Connect to Wi-Fi with strings ssid and password
   WiFi.begin(ssid, password);
+  int displayloadingbar = 0;
   //While it ISNT-connected it will delay and print Serial until connected. Easier way could be to use ext LCD to establish connection. 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
+    delay(500);
     Serial.println("Connecting to WiFi...");
+    display.clearDisplay();
+
+    //Simple dot-based loading screen to show that something is happening
+    if(displayloadingbar == 0) {
+      display.println("Connecting to WiFi");
+      displayloadingbar = displayloadingbar + 1;
+    }
+    else if(displayloadingbar == 1) {
+      display.println("Connecting to WiFi.");
+      displayloadingbar = displayloadingbar + 1;
+    }
+    else if(displayloadingbar == 2) {
+      display.println("Connecting to WiFi..");
+      displayloadingbar = displayloadingbar + 1;
+    }
+    else if(displayloadingbar == 3) {
+      display.println("Connecting to WiFi...");
+      displayloadingbar = 0;
+    }
+    display.display();
     blink();
+    display.setCursor(0, 10);
   }
 
-  //On Connect: 
-  Serial.println("Connected to WiFi. IP-address: ");
-  Serial.println(WiFi.localIP()); 
-
+  
+  
+  displayStatus();
 
   // Define web server routes
   server.on("/", HTTP_GET, handleRoot);
